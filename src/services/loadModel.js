5d3f4bcf6dc00ad02ec45e0e2344da7c88582ec3
Nopi <126689111+novyanywr@users.noreply.github.com>
2024-05-23 02:45:02 +0700

    Update loadModel.js

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,7 +1,7 @@
 const tf = require("@tensorflow/tfjs-node");
 
 async function loadModel() {
-  return tf.loadGraphModel(process.env.MODEL_URL);
+  return tf.loadGraphModel('https://storage.cloud.google.com/submission-mlgc-nopiani/submissions-model/model.json?authuser=1');
 }
 
 module.exports = loadModel;
